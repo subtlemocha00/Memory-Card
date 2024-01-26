1c9c30203c19f7abc57fb204c46fde4633e8fdee
subtlemocha00 <kturley@gmail.com>
2024-01-26 15:16:57 -0500

    added ./ to image file paths

--- src/assets/imageData.js
+++ src/assets/imageData.js
@@ -1,53 +1,61 @@
 const imageData = [
 	{
 		alt: "Beaver",
-		src: { medium: "src/assets/images/beaver.png" },
+		src: { medium: "./src/assets/images/beaver.png" },
 		id: "beaver",
 	},
-	{ alt: "Cat", src: { medium: "src/assets/images/cat.png" }, id: "cat" },
+	{ alt: "Cat", src: { medium: "./src/assets/images/cat.png" }, id: "cat" },
 	{
 		alt: "Chicken",
-		src: { medium: "src/assets/images/chicken.png" },
+		src: { medium: "./src/assets/images/chicken.png" },
 		id: "chicken",
 	},
-	{ alt: "Cow", src: { medium: "src/assets/images/cow.png" }, id: "cow" },
+	{ alt: "Cow", src: { medium: "./src/assets/images/cow.png" }, id: "cow" },
 	{
 		alt: "Crocodile",
-		src: { medium: "src/assets/images/crocodile.png" },
+		src: { medium: "./src/assets/images/crocodile.png" },
 		id: "crocodile",
 	},
-	{ alt: "Dog", src: { medium: "src/assets/images/dog.png" }, id: "dog" },
-	{ alt: "Duck", src: { medium: "src/assets/images/duck.png" }, id: "duck" },
+	{ alt: "Dog", src: { medium: "./src/assets/images/dog.png" }, id: "dog" },
+	{ alt: "Duck", src: { medium: "./src/assets/images/duck.png" }, id: "duck" },
 	{
 		alt: "Elephant",
-		src: { medium: "src/assets/images/elephant.png" },
+		src: { medium: "./src/assets/images/elephant.png" },
 		id: "elephant",
 	},
-	{ alt: "Fish", src: { medium: "src/assets/images/fish.png" }, id: "fish" },
+	{ alt: "Fish", src: { medium: "./src/assets/images/fish.png" }, id: "fish" },
 	{
 		alt: "Giraffe",
-		src: { medium: "src/assets/images/giraffe.png" },
+		src: { medium: "./src/assets/images/giraffe.png" },
 		id: "giraffe",
 	},
 	{
 		alt: "Goat",
-		src: { medium: "src/assets/images/goat.png" },
+		src: { medium: "./src/assets/images/goat.png" },
 		id: "goat",
 	},
 	{
 		alt: "Gorilla",
-		src: { medium: "src/assets/images/gorilla.png" },
+		src: { medium: "./src/assets/images/gorilla.png" },
 		id: "gorilla",
 	},
-	{ alt: "Horse", src: { medium: "src/assets/images/horse.png" }, id: "horse" },
-	{ alt: "Lion", src: { medium: "src/assets/images/lion.png" }, id: "lion" },
-	{ alt: "Pig", src: { medium: "src/assets/images/pig.png" }, id: "pig" },
+	{
+		alt: "Horse",
+		src: { medium: "./src/assets/images/horse.png" },
+		id: "horse",
+	},
+	{ alt: "Lion", src: { medium: "./src/assets/images/lion.png" }, id: "lion" },
+	{ alt: "Pig", src: { medium: "./src/assets/images/pig.png" }, id: "pig" },
 	{
 		alt: "Sheep",
-		src: { medium: "src/assets/images/sheep.png" },
+		src: { medium: "./src/assets/images/sheep.png" },
 		id: "sheep",
 	},
-	{ alt: "Zebra", src: { medium: "src/assets/images/zebra.png" }, id: "zebra" },
+	{
+		alt: "Zebra",
+		src: { medium: "./src/assets/images/zebra.png" },
+		id: "zebra",
+	},
 ];
 
 export default imageData;
