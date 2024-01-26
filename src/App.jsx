c71ce65be9a3ec4e98080aa2358672dac17e07a3
subtlemocha00 <kturley@gmail.com>
2024-01-26 14:08:36 -0500

    added LocalBoard functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min";
 import "./App.css";
 import Rules from "./components/Rules";
-import Board from "./components/Board";
+// import Board from "./components/Board";
+import BoardLocal from "./components/BoardLocal";
 
 function App() {
 	return (
-		<div className="container">
+		<div className="">
 			<Rules />
-			<Board />
+			{/* <Board /> */}
+			<BoardLocal />
 		</div>
 	);
 }
