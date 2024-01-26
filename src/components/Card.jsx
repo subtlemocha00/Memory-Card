95d3708e92f34fc48c5dfe04119e0de611d4a776
subtlemocha00 <kturley@gmail.com>
2024-01-26 14:08:16 -0500

    CSS styling/formatting

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,15 @@
 export default function Card({ image, caption, handleClick }) {
 	return (
 		<div
-			className="card col-6 col-sm-4 col-md-3 col-lg-fluid m-4 p-3 d-flex flex-column justify-content-center bg-transparent border"
+			className="card col-6 col-sm-4 col-md-3 col-lg-fluid m-2 p-3 d-flex flex-column justify-content-center bg-transparent border border-warning border-5"
 			onClick={handleClick}
 		>
 			<img
 				src={image}
 				alt="image"
-				height="175px"
-				className="rounded-pill p-2 border"
+				className="rounded-pill p-1 border border-warning border-5 bg-white img-fluid"
 			></img>
-			<span className="text-center p-2 text-white">{caption}</span>
+			<span className="text-center text-warning text-lg">{caption}</span>
 		</div>
 	);
 }
--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -1,8 +1,8 @@
 export default function Rules() {
 	return (
-		<>
+		<div className="bg-danger d-flex justify-content-center">
 			<button
-				className="btn btn-white border-dark"
+				className="btn btn-lg btn-warning m-3 col-6 fs-3 fw-bold text-primary"
 				type="button"
 				data-bs-toggle="collapse"
 				data-bs-target="#rules"
@@ -12,8 +12,10 @@ export default function Rules() {
 				Rules
 			</button>
 			<div className="collapse" id="rules">
-				<p>Click on each card only once! See if you can get to 25 points.</p>
+				<p className="p-2 m-0 text-white fs-4">
+					Click on each card only once! See if you can get to 25 points.
+				</p>
 			</div>
-		</>
+		</div>
 	);
 }
